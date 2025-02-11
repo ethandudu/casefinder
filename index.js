a81010f8d1d8feffe22414e066c575a33bdd4a92
Ethan <41381482+ethandudu@users.noreply.github.com>
2025-02-12 00:32:45 +0100

    base

---
+++ index.js
@@ -0,0 +1,82 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path')
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database(path.join(__dirname, 'db.sqlite3'), (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        initializeDatabase();
+    }
+});
+
+function initializeDatabase() {
+    db.serialize(() => {
+        db.run(`CREATE TABLE IF NOT EXISTS cases (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "city" INTEGER NOT NULL DEFAULT 0,
+            "section" VARCHAR(2) NOT NULL,
+            "plot" INTEGER NOT NULL,
+            "case" VARCHAR(15) NOT NULL,
+            "owner" VARCHAR(255) NULL DEFAULT NULL
+        )`);
+    });
+}
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            
+        },
+        show: false,
+        icon: path.join(__dirname, 'src/assets/logo.png')
+    })
+    win.maximize();
+    win.show();
+    win.loadFile('src/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+ipcMain.on('search', (event, arg) => {
+    const {searchtype, city, section, plot, owner} = arg;
+    console.log(`Search type: ${searchtype}`);
+    console.log(`City: ${city}`);
+    console.log(`Section: ${section}`);
+    console.log(`Plot: ${plot}`);
+    console.log(`Owner: ${owner}`);
+    let query, params;
+    if (searchtype === 'plot') {
+        query = `SELECT * FROM cases WHERE city = ? AND section = ? AND plot = ? LIMIT 50`;
+        params = [city, section, plot];
+    } else {
+        query = `SELECT * FROM cases WHERE city = ? AND owner = ? LIMIT 50`;
+        params = [city, owner];
+    }
+    
+    db.all(query, params, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            event.reply('search-results', {error: err.message});
+        } else {
+            if (rows.length === 0) {
+                event.reply('search-results', {error: 'No results found.'});
+            } else {
+                event.reply('search-results', {results: rows});
+            }
+        }
+    });
+    
+});
---
+++ src/js/index.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const searchTypeSelect = document.getElementById('searchtype');
+
+    if (searchTypeSelect) {
+        searchTypeSelect.addEventListener('change', (event) => {
+            console.log(`Search type changed to: ${event.target.value}`);
+            if (event.target.value === 'owner') {
+                //hide div with id "searchbycase"
+                document.getElementById('plotsearch').style.display = 'none';
+                document.getElementById('ownersearch').style.display = 'block';
+            } else {
+                //hide div with id "searchbyowner"
+                document.getElementById('ownersearch').style.display = 'none';
+                document.getElementById('plotsearch').style.display = 'block';
+            }
+        });
+    } else {
+        console.error('Element with id "searchtype" not found.');
+    }
+
+    const formButton = document.getElementById('searchbtn');
+
+    if (formButton) {
+        formButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            if (document.getElementById('searchtype').value === 'plot') {
+                //sanitize input
+                const city = document.getElementById('city').value;
+                const section = document.getElementById('section').value;
+                const plot = document.getElementById('plot').value;
+                window.electronAPI.sendToMain('search', {
+                    searchtype: 'plot',
+                    city,
+                    section,
+                    plot,
+                    owner: null
+                });
+            } else {
+                const city = document.getElementById('cityowner').value;
+                const owner = document.getElementById('owner').value;
+                window.electronAPI.sendToMain('search', {
+                    searchtype: 'owner',
+                    city,
+                    section: null,
+                    plot: null,
+                    owner
+                });
+            }
+            
+        });
+    }
+});
+
+window.electronAPI.receiveFromMain('search-results', (data) => {
+    //populate table with results
+    document.getElementById('results').style.display = 'block';
+    const table = document.getElementById('resultstablebody');
+    if (data.error) {
+        table.innerHTML = `<tr><td colspan="5">${data.error}</td></tr>`;
+    } else {
+        let tableRows = '';
+        data.results.forEach((result) => {
+            tableRows += `<tr>
+                <td>${result.city}</td>
+                <td>${result.section}</td>
+                <td>${result.plot}</td>
+                <td>${result.owner}</td>
+                <td>${result.case}</td>
+            </tr>`;
+            table.innerHTML = tableRows;
+        });
+        
+        table.style.display = 'block';
+
+    }
+});
